195c3c9e70791e609a0d91a48177f4fe8aa9057f
Kazuki Tobita <kazukiti201@gmail.com>
2018-09-22 04:18:11 +0900

    Add Socket.io

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const express = require('express')
-const io = require('socket.io')()
-
 const app = express()
+const http = require('http').Server(app)
+const io = require('socket.io')(http)
+const PORT = process.env.PORT || 7000
 
-app.get('/', (req, res) => {
+app.get('/' , (req, res) => {
   res.sendFile(__dirname + '/index.html')
 })
 
-app.listen(8080, () => {
+io.on('connection',(socket) => {
+  console.log('Socket.io is 繋がってる')
+})
+
+http.listen(PORT, () => {
   console.log('サーバ is 動いてる')
 })
