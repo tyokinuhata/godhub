d104236c9a99085b69a034fae951b060f0ec6b91
Kazuki Tobita <kazukiti201@gmail.com>
2018-09-22 19:30:00 +0900

    Modify analyze.js

--- word2vec/analyze.js
+++ word2vec/analyze.js
@@ -1,6 +1,6 @@
 const w2v = require( 'word2vec' )
 const search = process.argv[2].trim()
-console.log(search)
+
 w2v.loadModel(__dirname + '/vector.txt', (err, model) => {
   console.log(model.mostSimilar(search , 10))
   return model.mostSimilar(search , 10)
